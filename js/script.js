0810ab422aff52c77dd8a5fed98e331813fd8ef4
Prashant Sahani <132678599+prashant9984@users.noreply.github.com>
2024-03-07 09:38:45 +0530

    Add files via upload
    website in html css javaScript and bootstrap use

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header.scroll");
+    }else{
+        nav.classList.remove("header.scroll");
+    }
+}
+
+ 
